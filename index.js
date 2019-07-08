f8ac14478f12517f8cbbedde82c14ce05bfa65d7
Eldin van Hengst <enr.vanhengst@live.nl>
2019-07-08 23:00:53 +0200

    Update main

---
+++ index.js
@@ -0,0 +1,7 @@
+lottie.loadAnimation({
+    container: document.getElementById('bm'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: 'data.json' // the path to the animation json
+  });
